b3d99214959e6bf3b37afb6d0c56102c2dceac05
Jeroen <jeroent94@gmail.com>
2017-12-04 14:55:32 -0800

    'Implemented everything up until RetiredForagerBee.js'

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,16 @@
-class ForagerBee {
+class ForagerBee extends Bee {
   // TODO..
+  constructor(){
+  	super()
+  	this.age = 10
+  	this.job = 'find pollen'
+  	this.canFly = true
+  	this.treasureChest = []
+  }
+  forage(treasure){
+    this.treasureChest.push(treasure)
+  }
 };
+
+var forager = new ForagerBee()
+console.log(forager)
--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
+  constructor(){
+  	this.age = 40
+  	this.job = 'gamble'
+  	this.color = 'grey'
+  }
+
+  //How to override methods?
+  canFly(){
+  	return true
+   super.canFly()
+  }
+  forage(){
+  	return 'I am too old, let me play cards instead'
+  	super.forage()
+  }
 };
